7d69bae4523e306d7d70055840034f0fbcd39f57
HajarIH <languages0701@gmail.com>
2023-06-15 12:44:20 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #!/bin/bash
-int main(void)
-{ puts("Programming is like building a multilingual puzzle");
 
- return(0);
+int main(void)
+{
+       	puts("\"Programming is like building a multilingual puzzle";
+ return (0);
 	}
