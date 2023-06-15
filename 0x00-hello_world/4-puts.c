85f9476859cc2824113fadf5dbd05ee872560bb7
HajarIH <languages0701@gmail.com>
2023-06-15 14:32:35 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
-
 #include<stdio.h>
-
+/**
+* main - a program that prints a line using puts
+* return 0 : means a success
+*/
 int main(void)
 {
-       	puts("\"Programming is like building a multilingual puzzle");
- return (0);
-	}
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
-
+/*
+* main a program printing a phrase using printf
+*
+* return 0 means a success
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
