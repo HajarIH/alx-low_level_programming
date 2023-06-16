88019a4641260d0807f66df22f41eac84cea5e00
HajarIH <languages0701@gmail.com>
2023-06-16 10:26:35 -0700

    sign

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/* Main - function returning the sign of a number */
+/* return (0) means success */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n < 0)
+printf("%d is negatif/n", n);
+else
+if (n == 0)
+printf("%d is zero/n", n);
+else
+printf("%d is positif/n", n);
+return (0);
+}
