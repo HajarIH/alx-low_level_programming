1c8d0c4e8bed476e9449afc0c799072314bf8603
HajarIH <languages0701@gmail.com>
2023-06-16 10:30:45 -0700

    sign

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,11 +10,14 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n < 0)
+{
 printf("%d is negatif/n", n);
 else
 if (n == 0)
+{
 printf("%d is zero/n", n);
 else
 printf("%d is positif/n", n);
+}}
 return (0);
 }
