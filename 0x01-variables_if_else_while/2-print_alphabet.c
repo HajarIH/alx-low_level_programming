e2a61a40204f38840af6f41b37ea22b07c0f665e
HajarIH <languages0701@gmail.com>
2023-06-16 16:38:02 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /*
  * main return lower case alphabet
  * return 0 means success
  */
 int main(void)
 {
-	char alph[25] = "abcdefghijklmnopqrstuvwxyz";
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 for (i = 0; i < 26; i++)
