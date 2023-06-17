94fed2e5cdd566cca904712b67d5ab2eedcdf2b9
HajarIH <languages0701@gmail.com>
2023-06-17 08:36:52 -0700

    comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/*
+ * main fuction prints two digit combination
+ * main returns 0
+ * return 0 means success 
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (i < j)
+{
+putchar(i + 48);
+putchar(j + 48);
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
