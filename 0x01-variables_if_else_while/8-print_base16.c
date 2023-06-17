3423792b71046caf7fc9bdc3e33eba531715f096
HajarIH <languages0701@gmail.com>
2023-06-17 06:55:20 -0700

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,15 +5,15 @@
  */
 int main(void)
 {
-int i;
+	int i;
 
-for (i = 0; i < 16; i++)
-{
-if (i < 10)
-putchar(48+i);
-else 
-putchar(87+i);
-}
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+			putchar(48 + i);
+		else
+			putchar(87 + i);
+	}
 putchar('\n');
 return (0);
 }
