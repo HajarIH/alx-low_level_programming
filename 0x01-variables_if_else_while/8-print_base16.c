e34093068f3c29ba38639c82b771de01223c57b0
HajarIH <languages0701@gmail.com>
2023-06-17 06:39:52 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main return lower case alphabet
+ * return 0 means success
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 16; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
