e727c9e21e548cf885f2c303bde183309f44529f
HajarIH <languages0701@gmail.com>
2023-06-17 06:34:04 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * main return lower case alphabet in reverse
+ * return 0 means success
+ */
+int main(void)
+{
+char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+
+for (i = 0; i < 26; i++)
+{
+putchar(alph[26 - i]);
+}
+putchar('\n');
+return (0);
+}
