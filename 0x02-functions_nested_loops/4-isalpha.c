ee233739822c8f1285138b748a795c942f8faf24
HajarIH <languages0701@gmail.com>
2023-06-20 13:29:45 -0700

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _isalpha - checks if a character is alpha
- * c is the character to be checked
- * return 1 if c is a character and 0 if not
+ * @c: is the character to be checked
+ * Return: 1 if c is a character and 0 if not
  */
 int _isalpha(int c)
 {
