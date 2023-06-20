2fea89caafe69e7856733c2570115325e6732984
HajarIH <languages0701@gmail.com>
2023-06-20 09:55:24 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main - function prints putchar
+ *
+ * return 0 means success
+ */
+int main(void)
+{
+	putchar(_);
+	putchar(p);
+	putchar(u);
+	putchar(t);
+	putchar(c);
+	putchar(h);
+	putchar(a);
+	putchar(r);
+	putchar('\n');
+	return (0);
+}
