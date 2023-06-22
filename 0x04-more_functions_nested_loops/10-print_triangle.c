0dea4e95d242735e6d5a8380809d7aec24f4eef8
HajarIH <languages0701@gmail.com>
2023-06-22 14:10:24 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -21,6 +21,10 @@ void print_triangle(int size)
                         _putchar('#');
                         }
 		}
+		_putchar('\n');
         }
-        _putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
