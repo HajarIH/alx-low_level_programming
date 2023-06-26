64e28aa4a6eaec226b0697f28c66f3c6f68cfb9c
HajarIH <languages0701@gmail.com>
2023-06-26 06:39:56 -0700

    swape

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - a function that swipe 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int n;
+	n = *a;
+	*a = *b;
+	*b = n;
+}
