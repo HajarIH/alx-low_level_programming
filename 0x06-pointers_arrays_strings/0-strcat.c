125cb2e93c08bea4b4b127075322349f95d5b7c1
HajarIH <languages0701@gmail.com>
2023-06-29 07:47:09 -0700

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,15 +12,16 @@ char *_strcat(char *dest, char *src)
 
 	i = 0;
 	j = 0;
-	while (src[i] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while (dest[j] != '\0')
+	while (src[j] != '\0')
         {
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
 	dest[i] = '\0';
+	return (dest);
 }
