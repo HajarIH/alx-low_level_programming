8e6e1b9376bdd6fc0d7ba8a36ce82a7115f26a20
HajarIH <languages0701@gmail.com>
2023-07-17 17:57:34 +0100

    function

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,57 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
+#endif
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+3-function_like_macro.h [unix] (23:59 31/12/1969)                                                                                                                                      0,0-1 All
+"3-function_like_macro.h" [New]
+
