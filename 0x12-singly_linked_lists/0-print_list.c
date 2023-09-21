c7932bc77b59ec577e90b36e5ee81bf662430e9f
HajarIH <languages0701@gmail.com>
2023-09-21 12:49:45 -0700

    print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,11 +6,10 @@
  * @h: pointer to the list_t list to print
  * Return: the number of nodes printed
  */
-
 size_t print_list(const list_t *h)
 {
 	size_t A = 0;
-	
+
 	while (h)
 	{
 		if (h->str == NULL)
