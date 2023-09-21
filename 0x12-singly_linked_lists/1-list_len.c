20e34394fd62b3f0ce79d8f3d065e0420c5f0656
HajarIH <languages0701@gmail.com>
2023-09-21 13:46:06 -0700

    lenght

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -11,13 +11,10 @@ size_t list_len(const list_t *h)
 {
 	size_t L = 0;
 
-	if (h->next == NULL)
-		return (0);
-	else
-		while (!h->next)
-		{
-			L++;
-			h = h->next;
+	while (!h->next)
+	{
+		L++;
+		h = h->next;
 		}
 	return (L);
 }
