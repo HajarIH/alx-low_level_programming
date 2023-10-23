eee1298a66d55074004a31321bbc2b701468b5bc
HajarIH <languages0701@gmail.com>
2023-10-23 14:31:44 -0700

    free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2: a function that frees a list
+ * @head: a list to free
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head)
+	{
+		current = (*head)->next;
+		free(*head);
+		*head = current;
+	}
+	*head = NULL;
+}
