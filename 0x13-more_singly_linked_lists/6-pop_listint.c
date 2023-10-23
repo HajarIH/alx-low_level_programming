a726dc55fea12211838d691909256fa5bf225f1c
HajarIH <languages0701@gmail.com>
2023-10-23 14:47:43 -0700

    free head

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,12 +9,14 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *current;
+	int number;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
+	number = (*head)->n;
 	current = (*head)->next;
 	free(*head);
 	(*head)->n = current->n;
 	(*head)->next = current->next;
-	return ((*head)->n);
+	return (number);
 }
