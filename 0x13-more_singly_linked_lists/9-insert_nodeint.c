ffdebd36c906098d7cb61720ba35523625e65ced
HajarIH <languages0701@gmail.com>
2023-10-24 10:14:22 -0700

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,6 +23,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 	}
 	new->next = current;
-	current->next = next;
+	current->next = new;
 	return (new);
 }
