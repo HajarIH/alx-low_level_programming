078f196bcb845197fc124405363f023b3652e846
HajarIH <languages0701@gmail.com>
2023-10-24 09:49:01 -0700

    sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint: a function that sums elements of a list
+ * @head: the head of a list
+ * Return: the sum of a list
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *current = head;
+
+	if (!head)
+		return (0);
+	while (!current)
+	{
+		sum = sum + current->n;
+		current = current->index;
+	}
+	return (sum);
+}
