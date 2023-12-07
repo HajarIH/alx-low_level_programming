2a2e4686ed61cdd50e41821912406d943b77e9e3
HajarIH <languages0701@gmail.com>
2023-12-07 12:58:42 -0800

    insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint(h, n));
 	else
 	{
-		while (i < idx)
+		while (i < idx - 1)
 		{
 			i++;
 			if (!current)
