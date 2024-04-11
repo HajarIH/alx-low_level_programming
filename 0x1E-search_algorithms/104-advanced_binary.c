8d3d3d19d25467850ecb062094ee7767a0e4f982
HajarIH <languages0701@gmail.com>
2024-04-11 22:53:48 +0000

    advanced binary

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -5,26 +5,28 @@ int range_binary(int *array, int left, int right, int value)
 	int mid, i;
 
 	if (left <= right)
+	{
 		mid = left + (right - left) / 2;
-	else
-		return (-1);
-	printf("Searching in array: ");
-	for (i = left; i < right; i++)
-		printf("%d, ", array[i]);
-	printf("%d\n", array[i]);
-	if (array[mid] == value)
-	{
-		if (mid == left || array[mid - 1] != value)
-			return (mid);
+		
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+		
+		if (array[mid] == value)
+		{
+			if (mid == left || array[mid - 1] != value)
+				return (mid);
+			else
+				return (range_binary(array, left, mid, value));
+		}
 		else
-			return (range_binary(array, left, mid, value));
-	}
-	else
-	{
-		if (array[mid] > value)
-			return (range_binary(array, left, mid - 1, value));
-		else
-			return (range_binary(array, mid + 1, right, value));
+		{
+			if (array[mid] > value)
+				return (range_binary(array, left, mid - 1, value));
+			else
+				return (range_binary(array, mid + 1, right, value));
+		}
 	}
 	return (-1);
 }
